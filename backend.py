11db70022a2230cd92cf9f874bc9edd4a267d27f
Arun Ajay <arunajay42795@gmail.com>
2018-10-13 03:55:32 -0400

    Added google text-voice api w/audio file output

--- backend.py
+++ backend.py
@@ -1,6 +1,34 @@
 import requests
 import wikipedia
 
+###
+def synthesize_text(text):
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.types.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('output.mp3', 'wb') as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+###
+
+
+
+
 S = requests.Session()
 
 URL  = "https://en.wikipedia.org/w/api.php"
@@ -33,5 +61,6 @@ for place in PLACES:
     print(place['title'], place['dist']*.00621)
     place_list.append(place['title'])
 
-print(wikipedia.summary(place_list[0], sentences = 3))
-
+x = wikipedia.summary(place_list[0], sentences = 3)
+print(x)
+synthesize_text(x)
